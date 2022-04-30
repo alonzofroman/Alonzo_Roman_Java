e3ce044982a143ba329228deb9f9d940d776771a
Roman <alonzofroman@gmail.com>
2022-04-29 17:20:32 -0700

    Added converter switch cases

---
+++ ConverterInterface/src/main/java/com/company/ConverterSwitch.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import com.company.interfaces.Converter;
+
+public class ConverterSwitch implements Converter {
+
+    public String convertMonth(int monthNumber) {
+        String month;
+        switch(monthNumber) {
+            case 1: month = "January";
+            case 2: month = "February";
+            case 3: month = "March";
+            case 4: month = "April";
+            case 5: month = "May";
+            case 6: month = "June";
+            case 7: month = "July";
+            case 8: month = "August";
+            case 9: month = "September";
+            case 10: month = "October";
+            case 11: month = "November";
+            case 12: month = "December";
+        }
+        System.out.println(month);
+    }
+
+    public String convertDay(int dayNumber) {
+
+    }
+
+}
