9c72f12e4225a1fcee14aa46c3b9cd533fd785f1
Roman <alonzofroman@gmail.com>
2022-05-02 15:45:08 -0700

    Reasigned variables to allow both classes to run int the main application

--- ConverterInterface/src/main/java/com/company/ConverterApplication.java
+++ ConverterInterface/src/main/java/com/company/ConverterApplication.java
@@ -11,6 +11,7 @@ public class ConverterApplication {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+
     Converter ifConverter = new ConverterIf();
 
         System.out.println("Enter a number of a month (1-12). (If case)");
@@ -20,20 +21,31 @@ public class ConverterApplication {
         System.out.println("Enter a number of a day (1-7) (If case)");
         int dayNumber = Integer.parseInt(scan.nextLine());
         ifConverter.convertDay(dayNumber);
-    }
-
-    public static void main2(String[] args) {
-        Scanner scan = new Scanner(System.in);
 
         Converter switchConverter = new ConverterSwitch();
 
         System.out.println("Enter a number of a month (1-12) (Switch case)");
-        int monthNumber = Integer.parseInt(scan.nextLine());
+         monthNumber = Integer.parseInt(scan.nextLine());
         switchConverter.convertMonth(monthNumber);
 
         System.out.println("Enter a number of a day (1-7) (Switch case)");
-        int dayNumber = Integer.parseInt(scan.nextLine());
+         dayNumber = Integer.parseInt(scan.nextLine());
         switchConverter.convertDay(dayNumber);
 
     }
+
+//    public static void main2(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//
+//        Converter switchConverter = new ConverterSwitch();
+//
+//        System.out.println("Enter a number of a month (1-12) (Switch case)");
+//        int monthNumber = Integer.parseInt(scan.nextLine());
+//        switchConverter.convertMonth(monthNumber);
+//
+//        System.out.println("Enter a number of a day (1-7) (Switch case)");
+//        int dayNumber = Integer.parseInt(scan.nextLine());
+//        switchConverter.convertDay(dayNumber);
+//
+//    }
 }
