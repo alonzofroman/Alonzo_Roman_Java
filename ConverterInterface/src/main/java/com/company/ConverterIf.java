83b9559c19653b7e3d0a1c2b9b06938e5cd44985
Roman <alonzofroman@gmail.com>
2022-05-02 14:19:47 -0700

    Added converterif class for if methods

--- ConverterInterface/src/main/java/com/company/ConverterIf.java
+++ ConverterInterface/src/main/java/com/company/ConverterIf.java
@@ -1,4 +1,97 @@
 package com.company;
 
-public class ConverterIf {
+import com.company.interfaces.Converter;
+
+public class ConverterIf implements Converter {
+
+    public String convertMonth(int monthNumber) {
+        String month = null;
+        if (monthNumber == 1) {
+            month = "January";
+            return month;
+        }
+        else if (monthNumber == 2) {
+            month = "February";
+            return month;
+        }
+        else if (monthNumber == 3) {
+            month = "March";
+            return month;
+        }
+        else if (monthNumber == 4) {
+            month = "April";
+            return month;
+        }
+        else if (monthNumber == 5) {
+            month = "May";
+            return month;
+        }
+        else if (monthNumber == 6) {
+            month = "June";
+            return month;
+        }
+        else if (monthNumber == 7) {
+            month = "July";
+            return month;
+        }
+        else if (monthNumber == 8) {
+            month = "August";
+            return month;
+        }
+        else if (monthNumber == 9) {
+            month = "September";
+            return month;
+        }
+        else if (monthNumber == 10) {
+            month = "October";
+            return month;
+        }
+        else if (monthNumber == 11) {
+            month = "November";
+            return month;
+        }
+        else if (monthNumber == 12) {
+            month = "December";
+            return month;
+        }
+        else {
+            return "No month found";
+        }
+    }
+
+    public String convertDay(int dayNumber) {
+        String day = null;
+        if (dayNumber == 1) {
+            day = "Sunday";
+            return day;
+        }
+        else if (dayNumber == 2) {
+            day = "Monday";
+            return day;
+        }
+        else if (dayNumber == 3) {
+            day = "Tuesday";
+            return day;
+        }
+        else if (dayNumber == 4) {
+            day = "Wednesday";
+            return day;
+        }
+        else if (dayNumber == 5) {
+            day = "Thursday";
+            return day;
+        }
+        else if (dayNumber == 6) {
+            day = "Friday";
+            return day;
+        }
+        else if (dayNumber == 7) {
+            day = "Friday";
+            return day;
+        }
+        else {
+            return "No day found.";
+        }
+    }
+
 }
--- ConverterInterface/src/main/java/com/company/ConverterSwitch.java
+++ ConverterInterface/src/main/java/com/company/ConverterSwitch.java
@@ -37,7 +37,25 @@ public class ConverterSwitch implements Converter {
     }
 
     public String convertDay(int dayNumber) {
-
+        String day = null;
+        switch(dayNumber) {
+            case 1: day = "Sunday";
+            break;
+            case 2: day = "Monday";
+            break;
+            case 3: day = "Tuesday";
+            break;
+            case 4: day = "Wednesday";
+            break;
+            case 5: day = "Thursday";
+            break;
+            case 6: day = "Friday";
+            break;
+            case 7: day = "Saturday";
+            break; 
+        }
+        System.out.println(day);
+        return day;
     }
 
 }
