eed045b9faf09d0cbd673607c67ce5b39cff5a7d
Roman <alonzofroman@gmail.com>
2022-05-02 15:36:06 -0700

    Added functions for switch case converter

--- ConverterInterface/src/main/java/com/company/ConverterApplication.java
+++ ConverterInterface/src/main/java/com/company/ConverterApplication.java
@@ -13,19 +13,27 @@ public class ConverterApplication {
 
     Converter ifConverter = new ConverterIf();
 
-        System.out.println("Enter a number of a month (1-12).");
+        System.out.println("Enter a number of a month (1-12). (If case)");
         int monthNumber = Integer.parseInt(scan.nextLine());
         ifConverter.convertMonth(monthNumber);
 
-        System.out.println("Enter a number of a day (1-7)");
+        System.out.println("Enter a number of a day (1-7) (If case)");
         int dayNumber = Integer.parseInt(scan.nextLine());
         ifConverter.convertDay(dayNumber);
+    }
 
+    public static void main2(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        Converter switchConverter = new ConverterSwitch();
 
+        System.out.println("Enter a number of a month (1-12) (Switch case)");
+        int monthNumber = Integer.parseInt(scan.nextLine());
+        switchConverter.convertMonth(monthNumber);
 
-    Converter switchConverter = new ConverterSwitch();
+        System.out.println("Enter a number of a day (1-7) (Switch case)");
+        int dayNumber = Integer.parseInt(scan.nextLine());
+        switchConverter.convertDay(dayNumber);
 
-        
     }
 }
