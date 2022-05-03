4f2f21f6751a217439fdbf3dca791aa3cd26987a
Roman <alonzofroman@gmail.com>
2022-05-02 18:42:42 -0700

    Added tests for doubles

--- CalculatorObject/src/test/java/com/company/CalculatorTest.java
+++ CalculatorObject/src/test/java/com/company/CalculatorTest.java
@@ -45,8 +45,27 @@ public class CalculatorTest {
     public void shouldAddTwoDoubles() {
         double actualResult = calc.add(3.4, 2.3);
         double expectedResult = 5.7;
-        assertEquals(expectedResult, actualResult);
-
+        assertEquals(expectedResult, actualResult, 0.2);
     }
 
+    @Test
+    public void shouldSubtractTwoDoubles() {
+        double actualResult = calc.subtract(5.5, 0.5);
+        double expectedResult = 5;
+        assertEquals(expectedResult, actualResult, 0.2);
+    }
+
+    @Test
+    public void shouldMultiplyTwoDoubles() {
+        double actualResult = calc.multiply(6.7, 4.4);
+        double expectedResult = 29.48;
+        assertEquals(expectedResult, actualResult, 0.2);
+    }
+
+    @Test
+    public void shouldDivideTwoDoubles() {
+        double actualResult = calc.divide(10.8, 2.2);
+        double expectedResult = 4.909;
+        assertEquals(expectedResult, actualResult, 0.2);
+    }
 }
