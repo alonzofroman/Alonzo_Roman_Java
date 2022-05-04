d9b7bb01df8c25eea06343db27754e186ecf8783
Roman <alonzofroman@gmail.com>
2022-05-03 18:32:07 -0700

    Made farmer class inherit character class

--- RPGInheritance/src/main/java/com/company/Farmer.java
+++ RPGInheritance/src/main/java/com/company/Farmer.java
@@ -1,4 +1,22 @@
 package com.company;
 
-public class Farmer {
+public class Farmer extends Character {
+
+    protected boolean harvesting;
+    protected boolean plowing;
+
+    public Farmer(String name, int strength, int health, int stamina, int speed, int attackPower, boolean running, boolean arrested, boolean harvesting, boolean plowing) {
+        this.name = name;
+        this.strength = 75;
+        this.health = 100;
+        this.stamina = 75;
+        this.speed = 10;
+        this.attackPower = 1;
+        this.running = false;
+        this.arrested = false;
+        this.harvesting = false;
+        this.plowing = false;
+    }
+
+
 }
