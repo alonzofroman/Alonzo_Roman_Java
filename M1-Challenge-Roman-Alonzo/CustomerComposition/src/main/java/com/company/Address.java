93ea67f3dca5ed3f6920469f6456d993d8e77bd7
Roman <alonzofroman@gmail.com>
2022-05-04 18:52:35 -0700

    Added address class

---
+++ M1-Challenge-Roman-Alonzo/CustomerComposition/src/main/java/com/company/Address.java
@@ -0,0 +1,50 @@
+package com.company;
+
+public class Address {
+    protected String street;
+    protected String city;
+    protected String state;
+    protected int zipcode;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(int zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zipcode=" + zipcode +
+                '}';
+    }
+}
